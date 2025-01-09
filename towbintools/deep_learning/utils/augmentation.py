0d057c5f7dc0eee7d4ebb270bfbf8dcbf0d88c34
spsalmon <psalmonsacha@gmail.com>
2025-01-09 10:08:35 +0100

    typo

--- towbintools/deep_learning/utils/augmentation.py
+++ towbintools/deep_learning/utils/augmentation.py
@@ -105,7 +105,7 @@ def get_training_augmentation(normalization_type, **kwargs):
             train_transform.append(NormalizePercentile(kwargs["lo"], kwargs["hi"]))
 
     if kwargs["enforce_n_channels"]:
-        train_transform.append(EnforceNChannels(n_channels=kwargs["n_channels"]))
+        train_transform.append(EnforceNChannels(n_channels=kwargs["enforce_n_channels"]))
 
     return albu.Compose(train_transform)
 
@@ -124,7 +124,7 @@ def get_prediction_augmentation(normalization_type, **kwargs):
             prediction_transform.append(NormalizePercentile(kwargs["lo"], kwargs["hi"]))
     
     if kwargs["enforce_n_channels"]:
-        prediction_transform.append(EnforceNChannels(n_channels=kwargs["n_channels"]))
+        prediction_transform.append(EnforceNChannels(n_channels=kwargs["enforce_n_channels"]))
 
     return albu.Compose(prediction_transform)
 
@@ -165,7 +165,7 @@ def get_prediction_augmentation_from_model(model, enforce_n_channels=None):
             normalization_type=normalization_type,
             enforce_n_channels=enforce_n_channels,
         )
-        
+
     return preprocessing_fn
 
 
