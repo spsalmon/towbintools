87685d9171220b7a145bc7efd5969866b752cc37
spsalmon <psalmonsacha@gmail.com>
2025-12-08 15:38:57 +0100

    fixed functions to find out if images are stacks or not

--- towbintools/foundation/image_handling.py
+++ towbintools/foundation/image_handling.py
@@ -402,58 +402,6 @@ def get_image_size_metadata(file_path: str) -> Optional[dict]:
         return None
 
 
-def check_if_zstack(file_path: str) -> bool:
-    """
-    Determine whether the given OME-TIFF file represents a z-stack.
-
-    Checks the z-dimension size in the OME-TIFF metadata to determine
-    if the image is a z-stack (more than one z-plane).
-
-    Returns False if the file is not a valid OME-TIFF or the metadata cannot be read.
-
-    Parameters:
-        file_path (str): Path to the OME-TIFF image file.
-
-    Returns:
-        bool: True if the image is a z-stack (z_dim > 1), False otherwise or if the file is not a valid OME-TIFF or the metadata cannot be read.
-    """
-    try:
-        ome_metadata = ome_types.from_tiff(file_path).images[0].pixels  # type: ignore
-        zdim = ome_metadata.size_z
-        return zdim > 1
-    except Exception as e:
-        print(
-            f"Caught exception when trying to check if given file is a z-stack using OME-TIFF metadata in {file_path}: {e}"
-        )
-        return False
-
-
-def check_if_time_series(file_path: str) -> bool:
-    """
-    Determine whether the given OME-TIFF file represents a time series.
-
-    Checks the z-dimension size in the OME-TIFF metadata to determine
-    if the image is a time series (more than one time point).
-
-    Returns False if the file is not a valid OME-TIFF or the metadata cannot be read.
-
-    Parameters:
-        file_path (str): Path to the OME-TIFF image file.
-
-    Returns:
-        bool: True if the image is a time series (t_dim > 1), False otherwise or if the file is not a valid OME-TIFF or the metadata cannot be read.
-    """
-    try:
-        ome_metadata = ome_types.from_tiff(file_path).images[0].pixels  # type: ignore
-        tdim = ome_metadata.size_t
-        return tdim > 1
-    except Exception as e:
-        print(
-            f"Caught exception when trying to check if given file is a z-stack using OME-TIFF metadata in {file_path}: {e}"
-        )
-        return False
-
-
 def check_if_stack(
     file_path: str,
     channels_to_keep: Optional[list[int]] = None,
@@ -476,9 +424,10 @@ def check_if_stack(
     if dimensions is None:
         # try to infer it from the image shape
         shape = get_shape_from_tiff(file_path, channels_to_keep=channels_to_keep)
+        if shape is None:
+            shape = read_tiff_file(file_path, channels_to_keep=channels_to_keep).shape
         if channels_to_keep is None:
             channels_to_keep = []  # ensure it's a list
-        print(f"Inferred shape: {shape}")
         if len(shape) == 3 and len(channels_to_keep) == 1:
             # assume (z,y,x)
             is_stack = True
@@ -503,6 +452,56 @@ def check_if_stack(
         is_stack = (t_dim > 1) or (z_dim > 1)
 
     return is_stack, (z_dim, t_dim)
+
+
+def check_if_zstack(file_path: str) -> bool:
+    """
+    Determine whether the given OME-TIFF file represents a z-stack.
+
+    Checks the z-dimension size in the OME-TIFF metadata to determine
+    if the image is a z-stack (more than one z-plane).
+
+    Returns False if the file is not a valid OME-TIFF or the metadata cannot be read.
+
+    Parameters:
+        file_path (str): Path to the OME-TIFF image file.
+
+    Returns:
+        bool: True if the image is a z-stack (z_dim > 1), False otherwise or if the file is not a valid OME-TIFF or the metadata cannot be read.
+    """
+    try:
+        _, (z_dim, _) = check_if_stack(file_path)
+        return z_dim > 1
+    except Exception as e:
+        print(
+            f"Caught exception when trying to check if given file is a z-stack using OME-TIFF metadata in {file_path}: {e}"
+        )
+        return False
+
+
+def check_if_time_series(file_path: str) -> bool:
+    """
+    Determine whether the given OME-TIFF file represents a time series.
+
+    Checks the z-dimension size in the OME-TIFF metadata to determine
+    if the image is a time series (more than one time point).
+
+    Returns False if the file is not a valid OME-TIFF or the metadata cannot be read.
+
+    Parameters:
+        file_path (str): Path to the OME-TIFF image file.
+
+    Returns:
+        bool: True if the image is a time series (t_dim > 1), False otherwise or if the file is not a valid OME-TIFF or the metadata cannot be read.
+    """
+    try:
+        _, (_, t_dim) = check_if_stack(file_path)
+        return t_dim > 1
+    except Exception as e:
+        print(
+            f"Caught exception when trying to check if given file is a timeseries using OME-TIFF metadata in {file_path}: {e}"
+        )
+        return False
 
 
 def get_acquisition_date(file_path: str) -> Optional[datetime]:
